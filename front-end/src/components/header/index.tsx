970167136b2a801a539c4c390a7750cdfee3e6e5
Weriks Santos <werikscs@gmail.com>
2022-10-21 20:39:28 -0300

    feat: create header layout to mobile and desktop

--- front-end/src/components/header/index.tsx
+++ front-end/src/components/header/index.tsx
@@ -1,7 +1,7 @@
 import logoDark from "../../assets/logo/motors-shop-black.svg"
 import openedMenuIcon from "../../assets/opened-menu.svg"
 import closedMenuIcon from "../../assets/closed-menu.svg"
-import { HeaderStyled, NavStyled } from "./style"
+import { HeaderStyled } from "./style"
 import { useState } from "react"
 
 export function Header(): JSX.Element {
@@ -16,22 +16,38 @@ export function Header(): JSX.Element {
     return (
         <HeaderStyled>
             <section>
-                <img src={logoDark} alt="logo do site" />
-                <img className="menu-icon" src={menuIcon} alt="" onClick={toggleMenu}/>
+                <a href="#">
+                    <img src={logoDark} alt="" />
+                </a>
+                <button className="btn-menu-icon" onClick={toggleMenu}>
+                    <img src={menuIcon} alt="" />
+                </button>
             </section>
 
-           { IsMenuOpened && <NavStyled>
+           <nav className={IsMenuOpened ? 'nav opened' : 'nav'}>
                 <ul>
                     <li><a href="#">Carros</a></li>
                     <li><a href="#">Motos</a></li>
                     <li><a href="#">Leilão</a></li>
+                    <div className="divider"></div>
+                    {/* <li className="nav-internal login__register">
+                        <a href="#">Fazer Login</a>
+                        <button>Cadastrar</button>
+                    </li> */}
+                    <li className="nav-internal user__logged">
+                        <div className="user-name-abrev">SL</div>                            
+                        <nav className="internal-nav">
+                            <p>Samuel Leão</p>
+                            <ul>
+                                <li><a href="#">Editar Perfil</a></li>
+                                <li><a href="#">Editar Endereço</a></li>
+                                <li><a href="#">Minhas Compras</a></li>
+                                <li><a href="#">Sair</a></li>
+                            </ul>
+                        </nav>                        
+                    </li>
                 </ul>
-                <div></div>
-                <ul>
-                    <li><a href="#">Fazer Login</a></li>
-                    <li><button>Cadastrar</button></li>
-                </ul>
-            </NavStyled>}
+            </nav>
         </HeaderStyled>
     )
 }
--- front-end/src/components/header/style.ts
+++ front-end/src/components/header/style.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { getRandomColor } from "../../utils";
 
 export const HeaderStyled = styled.header`
     position: relative;
@@ -21,48 +22,199 @@ export const HeaderStyled = styled.header`
 
         padding: 0 16px;
 
-        .menu-icon {
+        .btn-menu-icon {
             padding: 16px;
 
             cursor: pointer;
+
+            background-color: transparent;
+
+            img {
+                pointer-events: none;
+            }
         }
     }
-`;
 
-export const NavStyled = styled.nav`
-    position: absolute;
-    top: 80px;
-
-    width: 100%;
-
-    box-shadow: 0px 40px 40px rgba(0, 0, 0, 0.09);
-
-    div {
-        width: 100%;
-
-        height: 1px;
-        background-color: ${({ theme }) => theme.color.greyScale.grey4};
-    }
-
-    ul {
-        width: 100%;
-
-        padding: 32px 16px;
-
-        display: flex;
-        flex-direction: column;
-
-        gap: 44px;
-
-        a {
-            color: ${({ theme }) => theme.color.greyScale.grey2};
-            font-size: ${({ theme }) => theme.typo.body1_600.size};
-            font-weight: ${({ theme }) => theme.typo.body1_600.weight};
-            font-family: ${({ theme }) => theme.typo.body1_600.fontFamily};
-        }
-
-        button {
+    .nav {
+        > ul {
             width: 100%;
+
+            padding: 32px 0;
+
+            display: none;
+            flex-direction: column;
+
+            position: absolute;
+            top: 80px;
+            left: 0;
+
+            box-shadow: 0px 40px 40px rgba(0, 0, 0, 0.09);
+
+            gap: 44px;
+
+            .divider {
+                height: 1px;
+                background-color: ${({ theme }) => theme.color.greyScale.grey4};
+            }
+
+            > li {
+                padding: 0 16px;
+            }
+
+            > li a {
+                color: ${({ theme }) => theme.color.greyScale.grey2};
+                font-size: ${({ theme }) => theme.typo.body1_600.size};
+                font-weight: ${({ theme }) => theme.typo.body1_600.weight};
+                font-family: ${({ theme }) => theme.typo.body1_600.fontFamily};
+
+                white-space: nowrap;
+            }
+
+            .nav-internal {
+                display: flex;
+                flex-direction: column;
+                gap: 8px;
+
+                width: fit-content;
+            }
+
+            .login__register {
+                gap: 44px;
+            }
+
+            .user__logged {
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+
+                height: 60px;
+
+                .user-name-abrev {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+
+                    width: 32px;
+                    height: 32px;
+
+                    border-radius: 16px;
+
+                    font-weight: ${({ theme }) => theme.typo.body2_500.weight};
+                    font-size: ${({ theme }) => theme.typo.body2_500.size};
+                    font-family: ${({ theme }) =>
+                        theme.typo.body2_500.fontFamily};
+
+                    color: ${({ theme }) => theme.color.greyScale.whiteFixed};
+                    background-color: ${({ theme }) =>
+                        getRandomColor(theme.color.randomColors)};
+                }
+
+                .internal-nav {
+                    position: relative;
+
+                    white-space: nowrap;
+
+                    > p {
+                        color: ${({ theme }) => theme.color.greyScale.grey2};
+                        font-weight: ${({ theme }) =>
+                            theme.typo.body2_500.weight};
+                        font-size: ${({ theme }) => theme.typo.body2_500.size};
+                        font-family: ${({ theme }) =>
+                            theme.typo.body2_500.fontFamily};
+
+                        cursor: pointer;
+                    }
+
+                    > ul {
+                        display: none;
+                        flex-direction: column;
+                        position: absolute;
+                        gap: 16px;
+
+                        top: 30px;
+                        left: -40px;
+
+                        width: 200px;
+
+                        padding: 22px;
+
+                        background-color: ${({ theme }) =>
+                            theme.color.greyScale.grey9};
+
+                        border-radius: 4px;
+                        box-shadow: 0px 4px 40px -10px rgba(0, 0, 0, 0.25);
+
+                        li a {
+                            color: ${({ theme }) =>
+                                theme.color.greyScale.grey2};
+                            font-size: ${({ theme }) =>
+                                theme.typo.body1_600.size};
+                            font-weight: ${({ theme }) =>
+                                theme.typo.body1_600.weight};
+                            font-family: ${({ theme }) =>
+                                theme.typo.body1_600.fontFamily};
+
+                            white-space: nowrap;
+                        }
+                    }
+                }
+
+                @media screen and (min-width: 800px) {
+                    .internal-nav {
+                        > ul {
+                            left: -65px;
+                        }
+                    }
+                }
+
+                :hover nav ul {
+                    display: flex;
+                }
+            }
+        }
+    }
+    .nav.opened {
+        > ul {
+            display: flex;
+        }
+    }
+
+    @media screen and (min-width: 800px) {
+        section {
+            .btn-menu-icon {
+                display: none;
+            }
+        }
+
+        .nav,
+        .nav.opened {
+            > ul {
+                padding: 0;
+
+                position: static;
+
+                display: flex;
+                flex-direction: row;
+                align-items: center;
+
+                box-shadow: none;
+
+                .divider {
+                    height: 80px;
+                    width: 2px;
+                    background-color: ${({ theme }) =>
+                        theme.color.greyScale.grey6};
+                }
+
+                li {
+                    padding: 0;
+                }
+
+                .nav-internal {
+                    flex-direction: row;
+                    padding-right: 60px;
+                }
+            }
         }
     }
 `;
